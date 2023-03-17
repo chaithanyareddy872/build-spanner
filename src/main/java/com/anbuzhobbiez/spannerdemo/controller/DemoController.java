8c97a08fafd45e36c01de46717123d30936a475e
chaitanya reddy <s.c.reddy@globallogic.com>
2023-03-17 10:27:42 +0530

    get method added

--- src/main/java/com/anbuzhobbiez/spannerdemo/controller/DemoController.java
+++ src/main/java/com/anbuzhobbiez/spannerdemo/controller/DemoController.java
@@ -4,10 +4,15 @@ import com.anbuzhobbiez.spannerdemo.model.Orders;
 import com.anbuzhobbiez.spannerdemo.repo.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -17,16 +22,21 @@ public class DemoController {
     @Autowired
     EmployeeRepository employeeRepository;
 
-    @GetMapping("/greet/{orderitem}")
-    public String greet(@PathVariable String orderitem){
+    @PostMapping("/greet/{orderitem}")
+    public ResponseEntity<?> greet(@PathVariable String orderitem){
         employeeRepository.save(new Orders(
                 UUID.randomUUID().toString(),
                 orderitem
         ));
-        return "you have ordered "+orderitem;
+        return ResponseEntity.ok().body("order "+orderitem+" saved");
+    }
+    @GetMapping("/getorders")
+    public ResponseEntity<?> getallorders(){
+        ArrayList<Orders> ordersArrayList= (ArrayList<Orders>) employeeRepository.findAll();
+        return  ResponseEntity.ok().body(ordersArrayList);
     }
     @GetMapping("/")
     public String greetings(){
-        return "hello";
+        return "its working";
     }
 }
