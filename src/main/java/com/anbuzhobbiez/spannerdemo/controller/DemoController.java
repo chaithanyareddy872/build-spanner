024fc782d3c7e286ae04ff7cab8c85ddf14e679f
chaitanya reddy <s.c.reddy@globallogic.com>
2023-05-17 14:51:42 +0530

    changed

--- src/main/java/com/anbuzhobbiez/spannerdemo/controller/DemoController.java
+++ src/main/java/com/anbuzhobbiez/spannerdemo/controller/DemoController.java
@@ -3,12 +3,9 @@ package com.anbuzhobbiez.spannerdemo.controller;
 import com.anbuzhobbiez.spannerdemo.model.Orders;
 import com.anbuzhobbiez.spannerdemo.repo.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -20,7 +17,6 @@ public class DemoController {
 
     @Autowired
     EmployeeRepository employeeRepository;
-
     @GetMapping("/order/{orderitem}")
     public ResponseEntity<?> saveitem(@PathVariable String orderitem){
         employeeRepository.save(new Orders(
